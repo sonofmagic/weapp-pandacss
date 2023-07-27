cd2a9a185a707ece053182233cabf7a2042bfb2d
ice breaker <1324318532@qq.com>
2023-07-27 23:57:29 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export const foo = 'bar'
+
+export const wait = (timeout?: number) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true)
+    }, timeout)
+  })
+}
+
+export * from '@/util'
