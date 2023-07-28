b1d95572eaa7c2a94ce6fc836a08f25c873293e2
ice breaker <1324318532@qq.com>
2023-07-28 21:38:38 +0800

    feat: add cli logger

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,12 @@
 import { createContext } from './core/context'
 const args = process.argv.slice(2)
 
-async function codegen() {
+async function main() {
   const ctx = await createContext()
-  ctx.codegen()
+  if (args[0] === 'codegen') {
+    // copy @weapp-core/escape and patch create css
+    ctx.codegen()
+  }
 }
 
-if (args[0] === 'codegen') {
-  // copy @weapp-core/escape and patch create css
-  codegen()
-}
+main()
--- src/core/context.ts
+++ src/core/context.ts
@@ -3,16 +3,32 @@ import { defu } from 'defu'
 import { getPandacssConfig } from './config'
 import { copyEscape } from './codegen'
 import { patch } from './patch'
+import { tick, quote, dedent } from './logger'
 import { ICreateContextOptions } from '@/types'
 import { getDefaults } from '@/defaults'
 
 export async function createContext(options?: ICreateContextOptions) {
   const opt = defu(options, getDefaults())
+
   const pandaConfig = await getPandacssConfig(opt.pandaConfig)
 
   async function codegen() {
-    await copyEscape(resolve(pandaConfig.config.outdir, 'weapp-panda'))
-    await patch(resolve(pandaConfig.config.outdir, 'helpers.mjs'))
+    const words: string[] = []
+    const outdir = pandaConfig.config.outdir
+    const weappPandaDir = resolve(outdir, 'weapp-panda')
+    const patchHelpersPath = resolve(outdir, 'helpers.mjs')
+    await copyEscape(weappPandaDir)
+    words.push(dedent`
+    ${tick} ${quote(outdir, '/weapp-panda')}: the core escape function for weapp
+    `)
+    await patch(patchHelpersPath)
+    words.push(dedent`
+    ${tick} ${quote(
+      outdir,
+      '/helpers.mjs'
+    )}: inject escape function into helpers
+    `)
+    console.log(words.filter(Boolean).join('\n'))
   }
   return {
     pandaConfig,
