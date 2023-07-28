0bc2bcf8395d45611f910f187993a8d3d23ba492
SonOfMagic <qq1324318532@gmail.com>
2023-07-28 18:02:50 +0800

    chore: add uni-app vue3 demo

---
+++ examples/uni-app-vue3/postcss.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    '@pandacss/dev/postcss': {},
+    // #\# 这种是 @csstools/postcss-cascade-layers 加的
+    '@csstools/postcss-cascade-layers': {},
+    'weapp-pandacss/postcss': {}
+  }
+}
