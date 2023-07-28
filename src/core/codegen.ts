0054a0e789e39738cca8f08f3f126b9bb47bc92c
SonOfMagic <qq1324318532@gmail.com>
2023-07-28 10:50:30 +0800

    feat: commit codegen fn

---
+++ src/core/codegen.ts
@@ -0,0 +1,34 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+
+export async function ensureDir(p: string) {
+  try {
+    await fs.access(p)
+  } catch {
+    await fs.mkdir(p, {
+      recursive: true
+    })
+  }
+}
+
+export function getWeappCoreEscapeDir() {
+  return path.dirname(require.resolve('@weapp-core/escape'))
+}
+// dirName: string = 'weapp-panda'
+export async function codegen(destDir: string) {
+  const result: string[] = []
+  const srcDir = getWeappCoreEscapeDir()
+  const filesnames = await fs.readdir(srcDir)
+  await ensureDir(destDir)
+
+  for (const filesname of filesnames) {
+    const src = path.resolve(srcDir, filesname)
+    const stats = await fs.stat(src)
+    if (stats.isFile()) {
+      const dest = path.resolve(destDir, filesname)
+      result.push(dest)
+      await fs.copyFile(src, dest)
+    }
+  }
+  return result
+}
---
+++ test/codegen.test.ts
@@ -0,0 +1,27 @@
+import path from 'node:path'
+import fs from 'node:fs'
+import { deleteAsync } from 'del'
+import { appRoot } from './util'
+import { getPandacssConfig } from '@/core/config'
+import { getWeappCoreEscapeDir, codegen } from '@/core/codegen'
+describe('codegen', () => {
+  it('has install WeappCoreEscape', () => {
+    const dir = getWeappCoreEscapeDir()
+    expect(dir).toBeDefined()
+  })
+
+  it('codegen to fixtures app', async () => {
+    const { config } = await getPandacssConfig({
+      cwd: appRoot
+    })
+
+    const target = path.resolve(appRoot, config.outdir, 'weapp-panda')
+    await deleteAsync([target])
+    expect(fs.existsSync(target)).toBe(false)
+    const res = await codegen(target)
+    expect(res.length).toBe(3)
+    for (const filename of res) {
+      expect(fs.existsSync(filename)).toBe(true)
+    }
+  })
+})
