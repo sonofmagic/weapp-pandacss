f4d68ee312d902b6acd2703181740b39cc516eff
ice breaker <1324318532@qq.com>
2023-07-29 13:14:33 +0800

    chore: upgrade taro demo

--- examples/taro-app/src/pages/index/index.tsx
+++ examples/taro-app/src/pages/index/index.tsx
@@ -1,7 +1,19 @@
-import { View, Text, Button } from '@tarojs/components'
+import { View, Text, Button, Input } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
-import { css } from '@styled-system/css'
+import { css, cx } from '@styled-system/css'
 import './index.scss'
+
+const cardStyles = css({
+  borderWidth: '1px',
+  borderRadius: '8px',
+  paddingX: '12px',
+  paddingY: '24px'
+})
+
+const Card = ({ className, ...props }) => {
+  const rootClassName = cx(cardStyles, className)
+  return <View className={rootClassName} {...props} />
+}
 
 const styles = css({
   backgroundColor: 'gainsboro',
@@ -20,6 +32,8 @@ export default function Index() {
   useLoad(() => {
     console.log('Page loaded.')
   })
+
+  const items = [1, 2, 3, 4, 5, 6]
 
   return (
     <View>
@@ -72,6 +86,158 @@ export default function Index() {
       >
         <Text>pinkpink</Text>
       </View>
+      <Card
+        className={css({
+          bg: 'cyan.800'
+        })}
+      >
+        啊啊啊啊啊啊啊啊
+      </Card>
+      <View
+        className={css({
+          bg: { base: 'red.500', _hover: 'red.700' }
+        })}
+      >
+        大大说
+      </View>
+      <View
+        className={css({
+          bg: { base: 'red.500', _hover: { _focus: 'red.700' } }
+        })}
+      >
+        大大说
+      </View>
+
+      <View
+        className={css({
+          '&[data-state=closed]': { color: 'red.300' },
+          '& > *': { margin: '2' }
+        })}
+      />
+
+      <View
+        className={css({
+          bg: 'red.500',
+          _hover: { bg: 'red.700' },
+          _active: { bg: 'red.900' }
+        })}
+      >
+        Hover me
+      </View>
+
+      <Text>
+        {items.map((item) => (
+          <Text key={item} className={css({ _first: { color: 'red.500' } })}>
+            {item}
+          </Text>
+        ))}
+      </Text>
+      <Text>
+        {items.map((item) => (
+          <Text
+            key={item}
+            className={css({
+              _even: { bg: 'blue' },
+              _odd: { bg: 'red' }
+            })}
+          >
+            {item}
+          </Text>
+        ))}
+      </Text>
+
+      <View
+        className={css({
+          _before: { content: '"👋"' }
+        })}
+      >
+        Hello
+      </View>
+
+      <Input
+        placeholder='Enter your name'
+        className={css({
+          _placeholder: { color: 'green.700' }
+        })}
+      />
+      <View
+        className={css({
+          _motionReduce: { transition: 'none' },
+          _motionSafe: { transition: 'all 0.3s' }
+        })}
+      >
+        Hello
+      </View>
+
+      <View
+        className={css({
+          bg: 'black',
+          _osDark: { bg: 'white' }
+        })}
+      >
+        Hello
+      </View>
+
+      <View
+        className={css({
+          bg: 'white',
+          _highContrast: { bg: 'black' }
+        })}
+      >
+        Hello
+      </View>
+
+      <View
+        className={css({
+          pb: '4',
+          _portrait: { pb: '8' }
+        })}
+      >
+        Hello
+      </View>
+
+      <View className='group'>
+        <Text className={css({ _groupHover: { bg: 'red.500' } })}>
+          Hover me
+        </Text>
+      </View>
+
+      <View>
+        <Text className='peer'>Hover me</Text>
+        <Text className={css({ _peerHover: { bg: 'red.500' } })}>
+          I'll change by bg
+        </Text>
+      </View>
+
+      <View dir='ltr'>
+        <View
+          className={css({
+            _ltr: { ml: '3' },
+            _rtl: { mr: '3' }
+          })}
+        >
+          Hello
+        </View>
+      </View>
+
+      <View
+        data-loading
+        className={css({
+          _loading: { bg: 'gray.500' }
+        })}
+      >
+        Hello
+      </View>
+
+      <dViewiv
+        data-orientation='horizontal'
+        className={css({
+          _horizontal: { bg: 'red.500' },
+          _vertical: { bg: 'blue.500' }
+        })}
+      >
+        Hello
+      </dViewiv>
     </View>
   )
 }
