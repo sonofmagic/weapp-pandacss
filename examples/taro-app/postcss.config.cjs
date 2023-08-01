a27ce63da04812f9827bef8a1ff861d14e413177
SonOfMagic <qq1324318532@gmail.com>
2023-08-01 16:01:07 +0800

    chore: fix taro app weapp and h5 env

--- examples/taro-app/postcss.config.cjs
+++ examples/taro-app/postcss.config.cjs
@@ -1,7 +1,13 @@
+
+const plugins = {
+  '@pandacss/dev/postcss': {},
+}
+// https://taro-docs.jd.com/docs/envs#processenvtaro_env
+if (process.env.TARO_ENV !== 'h5' && process.env.TARO_ENV !== 'rn') {
+  plugins['weapp-pandacss/postcss'] = {}
+}
+
+
 module.exports = {
-  plugins: {
-    '@pandacss/dev/postcss': {},
-    // #\# 这种是 @csstools/postcss-cascade-layers 加的
-    'weapp-pandacss/postcss': {}
-  }
+  plugins
 }
