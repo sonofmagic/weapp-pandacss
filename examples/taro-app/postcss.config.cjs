81dcb0b2fb470ff3cafd7a3a7ac9c6c746aa1081
ice breaker <1324318532@qq.com>
2023-08-23 15:00:42 +0800

    chore: taro tw and panda css demo upgrade

--- examples/taro-app/postcss.config.cjs
+++ examples/taro-app/postcss.config.cjs
@@ -1,6 +1,6 @@
 let plugins = {
   tailwindcss: {},
-  '@pandacss/dev/postcss': {}
+  '@pandacss/dev/postcss': {},
 }
 // https://taro-docs.jd.com/docs/envs#processenvtaro_env
 if (process.env.TARO_ENV !== 'h5' && process.env.TARO_ENV !== 'rn') {
@@ -9,7 +9,14 @@ if (process.env.TARO_ENV !== 'h5' && process.env.TARO_ENV !== 'rn') {
   plugins['weapp-pandacss/postcss'] = options
 }
 
-// plugins['autoprefixer'] = {}
+plugins['postcss-rem-to-responsive-pixel'] = {
+  // 32 意味着 1rem = 32rpx
+  rootValue: 32,
+  // 默认所有属性都转化
+  propList: ['*'],
+  // 转化的单位,可以变成 px / rpx
+  transformUnit: 'rpx',
+}
 
 module.exports = {
   plugins
