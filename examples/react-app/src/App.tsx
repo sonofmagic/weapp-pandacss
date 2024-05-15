b231fae83b69ed997669e09ca2654e05b6a9c19d
ice breaker <1324318532@qq.com>
2024-05-15 23:37:03 +0800

    chore: add react-ts

---
+++ examples/react-app/src/App.tsx
@@ -0,0 +1,9 @@
+import { css } from '../styled-system/css'
+
+function App() {
+  return (
+    <div className={css({ fontSize: '2xl', fontWeight: 'bold', color: 'sky.400' })}>Hello 🐼!</div>
+  )
+}
+
+export default App
